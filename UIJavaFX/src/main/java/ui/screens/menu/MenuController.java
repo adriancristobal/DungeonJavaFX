1415d6b4c6d02338c421355a7cfab05a6e28c908
Juan <juan.riquelme1@educa.madrid.org>
2023-01-25 10:22:45 +0100

    Avances elñ otro dia

--- UIJavaFX/src/main/java/ui/screens/menu/MenuController.java
+++ UIJavaFX/src/main/java/ui/screens/menu/MenuController.java
@@ -1,4 +1,25 @@
 package ui.screens.menu;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+
 public class MenuController {
+    @FXML
+    private ComboBox<String> dropDownSavedGames;
+
+    @FXML
+    void createDungeonAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void loadGameAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void newGameAction(ActionEvent event) {
+
+    }
 }
