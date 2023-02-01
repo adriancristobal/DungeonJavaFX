e114083f119922e769d5a29da4092eab7ceebcab
Lunnatik <tosigether598@gmail.com>
2023-02-01 10:30:22 +0100

    method classes

---
+++ UIJavaFX/src/main/java/ui/common/ScreenConstants.java
@@ -0,0 +1,8 @@
+package ui.common;
+
+public class ScreenConstants {
+
+    public static final String QUIT_GAME = "Quit the game";
+    public static final String CLOSE_GAME = "Close the game?";
+
+}
